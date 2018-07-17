b9fe1efc174e3fddcda37433a23049ded35d3d78
unknown <ccdelgadop@gmail.com>
2018-07-16 20:31:05 -0500

    comit a develop

---
+++ Proyecto1/businessdirectory/Api/urls.py
@@ -0,0 +1,10 @@
+from django.urls import include, path
+# from RuteandoApi.views import CitiesList, BusinessList, PostCities, PostBusiness
+from RuteandoApi import views
+
+urlpatterns = [
+    # path('empresa/', views.BusinessList.as_view()),
+    # path('ciudad/', views.CitiesList.as_view()),
+    # path('ciudad/<int:pk>/', views.CitiesDetail.as_view()),    
+    # path('empresa/<int:pk>/', views.BusinessDetail.as_view())
+]
--- Proyecto1/businessdirectory/Api/views.py
+++ Proyecto1/businessdirectory/Api/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
+from rest_framework import generics
+from RuteandoApi.models import Cities, Business
+from RuteandoApi.serializers import CitiesSerializer, BusinessSerializer
+
+# Create your views here.
+
+# class BusinessList(generics.ListCreateAPIView):
+#     queryset = Business.objects.all()
+#     serializer_class = BusinessSerializer
+
+# class CitiesList(generics.ListCreateAPIView):
+#     queryset = Cities.objects.all()
+#     serializer_class = CitiesSerializer
+
+
+#serializers
+
+# from rest_framework import serializers
+# from RuteandoApi.models import Cities, Business
+
+# class CitiesSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Cities
+#         fields = ('nombre','departamento','pais')
+
+# class BusinessSerializer(serializers.ModelSerializer):
+#     # ciudadid = serializers.PrimaryKeyRelatedField(many=True,read_only=True,allow_null=True)    
+#     class Meta:
+#         model = Business
+#         fields = ('nombre','direccion','ciudadid','telefono','descripcion')

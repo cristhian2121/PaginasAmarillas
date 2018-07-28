9d5435347973bbbde9d1c4861c786de98f856c7a
unknown <ccdelgadop@gmail.com>
2018-07-28 14:37:25 -0500

    Quitar el archivo de entorno interno y modificr vistas basadas en clases

--- Proyecto1/businessdirectory/Api/urls.py
+++ Proyecto1/businessdirectory/Api/urls.py
@@ -1,22 +1,22 @@
 from django.conf.urls import url, include
-from Api import views
+from Api.views import HomeView, 
 from django.views.generic import TemplateView
 
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url('', HomeView.as_view(), name='index'),
     url(r'^accounts/', include('registration.backends.hmac.urls')),
      #url(r'^accounts/registration/', views.registration, name='registration'),
 
 
-    url('home/', TemplateView.as_view(template_name='home.html')),
-     url('ciudad/', views.CiudadList.as_view()),
-    # path('ciudad/', views.CitiesList.as_view()),
-    url('usuario/<int:pk>/', views.CiudadDetail.as_view()),    
-    # path('empresa/<int:pk>/', views.BusinessDetail.as_view())
-    url('tipo/', views.TipoList.as_view()),
-    url('empresas/', views.EmpresasList.as_view()),
+    # url('home/', TemplateView.as_view(template_name='home.html')),
+    #  url('ciudad/', views.CiudadList.as_view()),
+    # # path('ciudad/', views.CitiesList.as_view()),
+    # url('usuario/<int:pk>/', views.CiudadDetail.as_view()),    
+    # # path('empresa/<int:pk>/', views.BusinessDetail.as_view())
+    # url('tipo/', views.TipoList.as_view()),
+    # url('empresas/', views.EmpresasList.as_view()),
     
-    url('vista2/', TemplateView.as_view(template_name='prueba.html'))
+    # url('vista2/', TemplateView.as_view(template_name='prueba.html'))
 
 ]
--- Proyecto1/businessdirectory/Api/views.py
+++ Proyecto1/businessdirectory/Api/views.py
@@ -3,40 +3,42 @@ from django.shortcuts import render, get_object_or_404, render_to_response
 from rest_framework import generics
 from django.views import generic
 from django.views.generic import TemplateView, ListView
-from Api.models import *
-from Api.serializers import *
+from Api.models import Ciudad
+# from Api.serializers import *
 
 # Create your views here.
-def index(request):
-    posts = Ciudad.objects.all()
-    return render(request, 'api/index.html')
-
-class aa(ListView):
+class HomeView(ListView):
     model = Ciudad
     template_name = 'api/index.html'
 
-#def registration(request):
-#    return render(request, 'registration/registration_form.html')
+
+
+def registration(request):
+   return render(request, 'registration/registration_form.html')
+
+# class aa(ListView):
+#     model = Ciudad
+#     template_name = 'api/index.html'
    
-class CiudadList(generics.ListCreateAPIView):  #get post
-    queryset = Ciudad.objects.all()
-    serializer_class = CiudadSerializer
+# class CiudadList(generics.ListCreateAPIView):  #get post
+#     queryset = Ciudad.objects.all()
+#     serializer_class = CiudadSerializer
 
-class CiudadDetail(generics.RetrieveDestroyAPIView):  #detele, put
-    queryset = Ciudad.objects.all()
-    serializer_class = CiudadSerializer
+# class CiudadDetail(generics.RetrieveDestroyAPIView):  #detele, put
+#     queryset = Ciudad.objects.all()
+#     serializer_class = CiudadSerializer
 
-class TipoList(generics.ListCreateAPIView):  #get post
-    queryset = Tipo.objects.all()
-    serializer_class = TipoSerializer
+# class TipoList(generics.ListCreateAPIView):  #get post
+#     queryset = Tipo.objects.all()
+#     serializer_class = TipoSerializer
 
-class EmpresasList(generics.ListCreateAPIView):  #get post
-    queryset = Empresas.objects.all()
-    serializer_class = EmpresasSerializer
+# class EmpresasList(generics.ListCreateAPIView):  #get post
+#     queryset = Empresas.objects.all()
+#     serializer_class = EmpresasSerializer
 
-class ViewHome(generic.DetailView):
-    # queryset = Ciudad
-    template_name = 'consultas/home/'
+# class ViewHome(generic.DetailView):
+#     # queryset = Ciudad
+#     template_name = 'consultas/home/'
 
-class ViewPrueba(TemplateView):
-    template_name = 'consultas/vista2/'
+# class ViewPrueba(TemplateView):
+#     template_name = 'consultas/vista2/'
